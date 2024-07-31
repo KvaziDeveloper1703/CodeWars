70c8524c64ce47222b992f100785f7309211173f
Viktor <viktor.sbruev.1999@gmail.com>
2024-07-31 07:51:57 +0300

    Five more tasks for difficulti level 8

---
+++ CW_8_8.py
@@ -0,0 +1,15 @@
+"""
+Write a program that returns the sum of the squares of all numbers in an array.
+https://www.codewars.com/kata/515e271a311df0350d00000f
+"""
+
+def sum_of_all_squared_numbers(given_array):
+    sum = 0
+    for number in given_array:
+        sum = sum + (number*number)
+    return sum
+
+my_array = [3,6,1,2,5]
+
+answer = square_sum(my_array)
+print(answer)
